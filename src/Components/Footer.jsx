387045f8c04027b75557a0d740ac73f05ff0b35e
ranvijaysingh198 <ranvijaysingh.rs79@gmail.com>
2022-01-21 02:49:43 +0530

    Initial Commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer(){
+    return <footer className=" bg-gradient-to-t from-cyan-500 to-blue-500 text-center text-xs p-3">
+        &copy;Copyright @ Ranvijay
+    </footer>
+};
+
+export default Footer;
---
+++ src/Components/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navigation(){
+    return (
+        <nav>
+            the nav
+        </nav>
+    )
+};
+
+export default Navigation;
